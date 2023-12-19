5c8b86210d8f63444e4ae35e039db0d43447b0c0
Shashank Kawle <skawle631@gmail.com>
2023-12-19 17:14:54 +0530

    added no-cors to default headers

--- src/core/httpHandler.js
+++ src/core/httpHandler.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 class HttpHandler {
 
     getDefaultHeaders() {
-        return { "Content-Type": "application/json" }
+        return { "mode": "no-cors", "Content-Type": "application/json" }
+        //return { "Content-Type": "application/json" }
         //return { "Content-Type": "application/java" }
     }
     getDefaultMultipartHeaders() {
